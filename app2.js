ec8adfaa1c94bdeedc9b69aefd9b83cdaffb1423
Muhammad Imran Khan <65187828+imrankhan2k2@users.noreply.github.com>
2020-07-22 23:12:27 +0400

    Created app2.js

---
+++ app2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express'),
+    app2 = express();
+
+app2.set('views', 'views');
+app2.set('view engine', 'pug');
+
+app2.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app2.listen(8070);
+module.exports.getApp = app2;
